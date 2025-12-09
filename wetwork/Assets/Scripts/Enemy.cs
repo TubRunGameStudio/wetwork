2087cf98b4029a625707f6823a344f036157d4a4
TubRunGameStudio <157238645+TubRunGameStudio@users.noreply.github.com>
2025-12-09 14:27:25 +0100

    Enemy Animate based on direction of movement

--- wetwork/Assets/Scripts/Enemy.cs
+++ wetwork/Assets/Scripts/Enemy.cs
@@ -6,10 +6,12 @@ using UnityEngine.Rendering.Universal.Internal;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] List<GameObject> path;
+    [SerializeField] private Animator animator;
 
     private NavMeshAgent agent;
     private GameObject destination;
     private int index;
+    private Vector3 prevPos;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -41,5 +43,25 @@ public class Enemy : MonoBehaviour
         }
 
         agent.SetDestination(destination.transform.position);
+
+        // animations
+        SetAnimation(transform.position, prevPos);
+        prevPos = transform.position;
+    }
+
+    private void SetAnimation(Vector3 curr, Vector3 prev)
+    {
+        float diffX = curr.x - prev.x;
+        float diffY = curr.y - prev.y;
+
+        if (diffX > 0 && diffY < 0)
+            animator.SetInteger("Direction", 1);
+        else if (diffX > 0 && diffY > 0)
+            animator.SetInteger("Direction", 2);
+        else if (diffX < 0 && diffY > 0)
+            animator.SetInteger("Direction", 3);
+        else if (diffX < 0 && diffY < 0)
+            animator.SetInteger("Direction", 4);
+
     }
 }
