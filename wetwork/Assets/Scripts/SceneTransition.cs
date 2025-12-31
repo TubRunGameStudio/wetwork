58042560e7279782ed325f6aa056736a3e47ff4c
TubRunGameStudio <157238645+TubRunGameStudio@users.noreply.github.com>
2025-12-31 14:28:22 +0100

    Added tooltip to scene transition

--- wetwork/Assets/Scripts/SceneTransition.cs
+++ wetwork/Assets/Scripts/SceneTransition.cs
@@ -1,15 +1,24 @@
+using System.Runtime.CompilerServices;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneTransition : MonoBehaviour, Interactable
 {
     [SerializeField] private string sceneTransitionName;
+    [SerializeField] GameObject tooltip;
+
+    void Update()
+    {
+        tooltip.transform.position = Camera.main.WorldToScreenPoint(transform.position);
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "Player")
         {
             PlayerController.PLAYER.SetExitSceneTransition(this);
+            tooltip.SetActive(true);
         }
     }
 
@@ -18,6 +27,7 @@ public class SceneTransition : MonoBehaviour, Interactable
         if (collision.tag == "Player")
         {
             PlayerController.PLAYER.SetExitSceneTransition(null);
+            tooltip.SetActive(false);
         }
     }
 
