48f54d5964a0e20a0e5d3fa8d0bbe1e5faed694c
TubRunGameStudio <157238645+TubRunGameStudio@users.noreply.github.com>
2026-01-06 12:54:45 +0100

    Attach explosion to grenade

---
+++ wetwork/Assets/Scripts/Weapons/Explosion.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+    [SerializeField] Animator animator;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+    }
+}
--- wetwork/Assets/Scripts/Weapons/Grenade_Projectile.cs
+++ wetwork/Assets/Scripts/Weapons/Grenade_Projectile.cs
@@ -3,10 +3,18 @@ using UnityEngine;
 
 public class Grenade_Projectile : Projectile
 {
-
+    [SerializeField] GameObject explosion;
     protected override IEnumerator ArcMove()
     {
         yield return base.ArcMove();
-        Debug.Log("Boom!");
+        Explode();
+    }
+
+    private void Explode()
+    {
+        GameObject expl = GameObject.Instantiate(explosion);
+        expl.transform.position = transform.position;
+        Destroy(gameObject);
+        Debug.Log("BOOM!");
     }
 }
