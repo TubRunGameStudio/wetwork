71726daf9ee69cb0d5a644501a98faf70a2af566
TubRunGameStudio <157238645+TubRunGameStudio@users.noreply.github.com>
2026-01-28 10:12:17 +0100

    Added minimap range for radio tower

---
+++ wetwork/Assets/Scripts/Interactables/RadioRange.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class RadioRange : MonoBehaviour
+{
+    [SerializeField] LineRenderer lineRenderer;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        DrawCircle(100, 10, transform.position);
+    }
+
+    private void DrawCircle(int steps, float radius, Vector3 center)
+    {
+        lineRenderer.positionCount = steps;
+        for(int i = 0; i < steps; i++)
+        {
+            float circumferenceProgress = (float)i / steps;
+            float currentRadian = circumferenceProgress * 2 * Mathf.PI;
+            float xScaled = Mathf.Cos(currentRadian);
+            float yScaled = Mathf.Sin(currentRadian);
+
+            float x = xScaled * radius;
+            float y = yScaled * radius;
+            Vector3 currentPosition = new Vector3(x, y, 0) + center;
+            lineRenderer.SetPosition(i, currentPosition);
+        }
+    }
+}
